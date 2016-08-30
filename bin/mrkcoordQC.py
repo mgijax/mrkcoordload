a8ec4294a67dde21d4ac3822080042763b3bd089
Lori Corbani <lori.corbani@jax.org>
2016-08-30 13:42:37 -0400

    TR11083/Retire/Archive Nomenclature Table (NOM_Marker)

--- bin/mrkcoordQC.py
+++ bin/mrkcoordQC.py
@@ -917,7 +917,7 @@ def createMirbaseDeleteReport ():
 	into temp mkrs
 	from ACC_Accession a, MRK_Marker m
 	where m._Organism_key = 1
-	and m._Marker_Status_key in (1,3)
+	and m._Marker_Status_key = 1
 	and m._Marker_key = a._Object_key
 	and a._MGIType_key = 2
 	and a._LogicalDB_key = 1
